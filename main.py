bd1e2bc65d4ffda37ef88260d1b7c4c57fab6601
JakubMielczarekPP <jakub.mielczarek.0@gmail.com>
2023-11-12 17:31:12 +0100

    init

---
+++ main.py
@@ -0,0 +1,31 @@
+# szeregowanie zadan
+def pcmax_greedy(processes, processors):
+    # posortuj zadania malejaco
+    processes.sort(reverse=True)
+
+    # utowrz tablice z zaplanowanymi procesami
+    schedules = [[] for _ in range(processors)]
+
+    for process in processes:
+        # znalezienie procesora z najkrotszym czasem wykonywania i przydzielenie mu procesu
+        shortest_processor = min(schedules, key=sum)
+        shortest_processor.append(process)
+
+    # oblicz laczny czas wykonywania dla procesorow i znajdz czas ostatniego procesora
+    end_times = [sum(processor) for processor in schedules]
+    max_end_time = max(end_times)
+
+    return max_end_time
+
+# lista zadan/procesow z pliku
+file = open("procesy.txt", "r")
+content = file.read().split()
+processes = [int(i) for i in content]
+
+# zdefiniuj ilosc procesorw przez input
+processors = 0
+while (processors < 1):
+    processors = int(input("Podaj ilość procesorów: "))
+
+end_time = pcmax_greedy(processes, processors)
+print("Ostatni proces zakonczy sie po:", end_time)
